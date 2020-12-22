2c510bf021942279f9b7f53e489924ea52447338
mujibbur741 <mujiburr741@gmail.com>
2020-12-22 15:11:39 +0700

    Log Changes[development] + implement ChatsAdapter.kt

---
+++ app/src/main/java/id/cikup/couriermanagementsystem/data/model/ChatsModel.kt
@@ -0,0 +1,7 @@
+package id.cikup.couriermanagementsystem.data.model
+
+data class Message(
+        val sentBy: String? = "",
+        val message: String? = "",
+        val messageTime: Long? = 0
+)
--- app/src/main/java/id/cikup/couriermanagementsystem/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/id/cikup/couriermanagementsystem/ui/dashboard/DashboardFragment.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 import id.cikup.couriermanagementsystem.R
+import id.cikup.couriermanagementsystem.data.model.Message
 import id.cikup.couriermanagementsystem.helper.OnBackPressedListener
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
-class DashboardFragment : Fragment(), OnBackPressedListener {
+class DashboardFragment : Fragment(), OnBackPressedListener, View.OnClickListener {
 
+    private val firebaseDb = FirebaseFirestore.getInstance()
+
+
+    private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    private val conversationAdapter = ConversationAdapter(arrayListOf(), userId)
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -19,7 +31,68 @@ class DashboardFragment : Fragment(), OnBackPressedListener {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        kirimChatDasboardFragmentIV.setOnClickListener(this)
+
+        chatDashboardFragmentRV.apply {
+            setHasFixedSize(false)
+            layoutManager = LinearLayoutManager(context)
+            adapter = conversationAdapter
+        }
+
+        getMessage()
+    }
+
+
     override fun onBackPressed() {
         this.requireActivity().moveTaskToBack(true)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.kirimChatDasboardFragmentIV -> {
+                if (!chatDashboardFragmentEDT.text.isNullOrEmpty()) {
+                    val message = Message(userId, chatDashboardFragmentEDT.text.toString(), System.currentTimeMillis())
+                    firebaseDb.collection("Chats")
+                            .document("2")
+                            .collection("Message")
+                            .document()
+                            .set(message)
+                    chatDashboardFragmentEDT.setText("", TextView.BufferType.EDITABLE)
+                }
+            }
+        }
+    }
+
+
+    fun getMessage() {
+        firebaseDb.collection("Chats")
+                .document("2")
+                .collection("Message").orderBy("messageTime")
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    if (firebaseFirestoreException != null) {
+                        firebaseFirestoreException.printStackTrace()
+                        return@addSnapshotListener
+                    } else {
+                        if (querySnapshot != null) {
+                            for (change in querySnapshot.documentChanges) {
+                                when (change.type) {
+                                    DocumentChange.Type.ADDED -> {
+                                        val message = change.document.toObject(Message::class.java)
+                                        if (message != null) {
+                                            conversationAdapter.addMessage(message)
+                                            chatDashboardFragmentRV.post {
+                                                chatDashboardFragmentRV.smoothScrollToPosition(conversationAdapter.itemCount - 1)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
     }
 }
